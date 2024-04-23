c55594100903070cbd5187a9a5e6be53d6fcc10b
an920107 <an920107@gmail.com>
2024-04-23 21:15:32 +0800

    feat: quick sort.

--- src/core/sorting.ts
+++ src/core/sorting.ts
@@ -1,3 +1,5 @@
+import { start } from "repl";
+
 export interface SortingMethod {
     name: string,
     numbers: Array<number>,
@@ -94,16 +96,74 @@ export class QuickSort implements SortingMethod {
     name = "Quick Sort";
     numbers: number[];
 
+    stack: Array<number> = [];
+    begin = 0;
+    end = 0;
+    left = 0;
+    right = 0;
+    pivot = 0;
+
+    state = 0;
+
     isCompleted() {
-        return true;
+        return this.stack.length === 0 && this.state === 0;
     }
 
     next() {
+        if (this.isCompleted()) return;
 
+        switch (this.state) {
+            case 0:
+                this.end = this.stack.pop()!;
+                this.begin = this.stack.pop()!;
+                if (this.begin < this.end) {
+                    this.pivot = this.numbers[this.begin];
+                    this.left = this.begin + 1;
+                    this.right = this.end;
+                    this.state = 1;
+                } else {
+                    break;
+                }
+            case 1:
+                if (this.right > this.begin && this.numbers[this.right] >= this.pivot) {
+                    this.right--;
+                    break;
+                } else {
+                    this.state = 2;
+                }
+            case 2:
+                if (this.left <= this.right && this.numbers[this.left] <= this.pivot) {
+                    this.left++;
+                    break;
+                } else {
+                    this.state = 3;
+                }
+            case 3:
+                if (this.left < this.right) {
+                    swap(this.numbers, this.left, this.right);
+                    this.state = 1;
+                } else {
+                    if (this.begin < this.right) {
+                        swap(this.numbers, this.begin, this.right);
+                        this.stack.push(this.begin);
+                        this.stack.push(this.right - 1);
+                    }
+                    if (this.right < this.end) {
+                        this.stack.push(this.right + 1);
+                        this.stack.push(this.end);
+                    }
+                    this.state = 0;
+                }
+                break;
+            default:
+                break;
+        }
     }
 
     constructor(numbers: Array<number>) {
         this.numbers = numbers;
+        this.stack.push(0);
+        this.stack.push(numbers.length - 1);
     }
 }
 
